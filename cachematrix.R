1d2f4a973f7c02ba372feee1bfb80726bccff9d9
kfmauric <kyle.maurice@gmail.com>
2015-08-18 10:23:35 -0700

    Update the comments to be more descriptive of data structure.

--- cachematrix.R
+++ cachematrix.R
@@ -22,7 +22,8 @@ makeCacheMatrix <- function(x = matrix()) {
     invCache <<- x
   }
 
-  # don't understand that this is doing
+  # Function returns a list containing functions that can be accessed
+  # The data is stored in the contents of the functions
    list(set = set, get = get,
        setcache = setcache,
        getcache = getcache)
@@ -30,14 +31,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# This function takes an object created by the makeCacheMatrix function and checks for the existance of # a chached version of the inverse. If no chached vesion of the inverse exists the invers is generated # using solve and the object is updated with a chache of the inverse. In both cases the inverse of the # matrix is returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  #check the oject to see if there is a cacheed version of the matrix that is already stored
-  # if there is no cahed version stored compute the inverse and cache the inverse in the object
-
-  #check to see if there is slready a cache imbedded in the object
+  # Check the object to see if there is a cacheed version of the matrix that is already stored
+  # If there is no cahed version stored compute the inverse and cache the inverse in the object
+  # Check to see if there is slready a cache imbedded in the object
+  # If there is already a chaced value returnit
+  # Else compute the value and set the object cache
 
   cache <- x$getcache()
   if (is.null(cache)) {
@@ -49,8 +50,6 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     result <- x$getcache()
   }
-  #if there is already a chaced value returnit
-  #else compute the value and set the object cache
-
+  ## Return a matrix that is the inverse of 'x'
   result
 }
